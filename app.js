7b5cba4a27267916bc04115d27c55118efbf35c2
havillon <84211284+havillon@users.noreply.github.com>
2021-09-22 15:59:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const fs = require('fs');
+var id = 0;
+
+const port = 5000;
+app.use(express.static('public'));
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/', urlencodedParser, function (req, res) {
+    fs.readFile(__dirname + '/public/itens.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+        obj = JSON.parse(data); //transforma o conteudo do arquivo em objeto
+        obj.lista.push({id: id, item:req.body.item}); //insere o novo item que veio do formulario
+        json = JSON.stringify(obj); //transforma o objeto em json de volta
+        fs.writeFile(__dirname + '/public/itens.json', json, ()=>{}); //sobrescreve o json antigo
+    }});
+    id+=1;
+    res.sendFile(__dirname + '/public/index.html');
+ })
+
+app.listen(port);
+
+//por enquanto só pega o que vem do formulário e adiciona no arquivo json
